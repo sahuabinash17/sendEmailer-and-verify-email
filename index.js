0362d3115e1e0dbc83d668acdc54588780c11cec
sahuabinash17 <103118899+sahuabinash17@users.noreply.github.com>
2022-04-06 18:07:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,36 +8,7 @@
 
 // app.use(express.json());
 
-// app.post('/',(req,res)=>{
-//     const options={
-//         from:"sahuabinash17@gmail.com",
-//         to:"lenkaharekrushna1@gmail.com",
-//         subject:"test nodemailer",
-//         text:"562489"
-//     };
-    
-//     const transporter = nodemailer.createTransport({
-//         service:"gmail",
-//         auth:{
-//             user:"sahuabinash17@gmail.com",
-//             pass:"8637294845"
-//         }
-//     });
-    
-// transporter.sendMail(options, (err,info) =>{
-//     if(err){
-//         console.log(err);
-//         return;
-//     }
-//     console.log("sent: " + info.response);
-// })
-    
-// })
 
-// app.listen(port,()=>{
-
-//     console.log(`App running on port ${port}`)
-// })
 const app = require("./server")
 
 app.listen(3000,()=>{
